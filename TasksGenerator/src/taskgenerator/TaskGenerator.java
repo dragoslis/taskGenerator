a891d57b7bf23f429e6e80f5dd4756ef5d725f56
Dragoslis-Thanasis <a.dragoslis@aade.gr>
2023-02-01 14:47:52 +0200

    debugging app continue

---
+++ TasksGenerator/src/taskgenerator/TaskGenerator.java
@@ -0,0 +1,12 @@
+package taskgenerator;
+
+// init application
+public class TaskGenerator {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            new MainFrame().setVisible(true);
+        });
+    }
+    
+}
